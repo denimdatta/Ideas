c18adb97191ce4934c4cbab9c6152a11217d916f
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-31 03:43:27 -0800

    Add Form Post

--- resources/views/ideas.blade.php
+++ resources/views/ideas.blade.php
@@ -1,5 +1,7 @@
 <x-layout>
-    <form action="">
+    <form method="POST" action="/ideas">
+        @csrf
+
         <div class="col-span-full">
             <label for="idea" class="block text-sm/6 font-medium text-gray-900">New Idea</label>
             <div class="mt-2">
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('ideas');
 });
+
+Route::post('/ideas', function () {
+    $idea = request('idea');
+    session()->push('ideas', $idea);
+
+    return redirect('/');
+});
