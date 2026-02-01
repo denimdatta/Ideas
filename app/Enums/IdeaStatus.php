7faf90cedf3e1b0ac69bd5d1ef73555a1fbe23cd
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-31 17:22:56 -0800

    Add More Idea Status

--- app/Enums/IdeaStatus.php
+++ app/Enums/IdeaStatus.php
@@ -4,6 +4,7 @@ namespace App\Enums;
 
 enum IdeaStatus: string
 {
+    case PENDING = 'pending';
     case OPEN = 'open';
     case IN_PROGRESS = 'in_progress';
     case CANCELED = 'canceled';
@@ -12,6 +13,7 @@ enum IdeaStatus: string
     public function getDisplayName(): string
     {
         return match ($this) {
+            self::PENDING => 'Pending',
             self::OPEN => 'Open',
             self::IN_PROGRESS => 'In Progress',
             self::CANCELED => 'Canceled',
