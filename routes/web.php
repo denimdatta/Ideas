e1ce0dcfdc102c91be34c1929c1665607008d013
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-31 16:53:53 -0800

    Get Ideas from Model

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Enums\IdeaStatus;
+use App\Models\Idea;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $ideas = session('ideas', []);
+    $ideas = Idea::all();
 
     return view('ideas', [
         'ideas' => $ideas,
@@ -11,8 +13,10 @@ Route::get('/', function () {
 });
 
 Route::post('/ideas', function () {
-    $idea = request('idea');
-    session()->push('ideas', $idea);
-
+    $idea = Idea::factory()->create();
+    $idea->description = request('idea');
+    $idea->status = IdeaStatus::OPEN;
+    $idea->save();
+    
     return redirect('/');
 });
