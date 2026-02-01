ce02526b8c412e0a0b936917bed080c86729b96c
Denim Datta 👻 <d.dat08@gmail.com>
2026-02-01 00:41:57 -0800

    Fix Line Break and Whitespace Append on Edit Issue

--- resources/views/ideas/edit.blade.php
+++ resources/views/ideas/edit.blade.php
@@ -39,12 +39,11 @@
             <div class="mt-2">
                 <textarea id="idea"
                           name="idea"
-                          rows="3"
                           class="block w-full rounded-md bg-white px-3 py-1.5 text-base
                                 text-gray-900 outline-1 -outline-offset-1 outline-gray-300
                                 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2
-                                focus:outline-indigo-600 sm:text-sm/6">{{ old('idea', $idea->description) }}
-                </textarea>
+                                focus:outline-indigo-600 sm:text-sm/6"
+                          rows="3">{{ old('idea', $idea->description) }}</textarea>
             </div>
         </div>
 
