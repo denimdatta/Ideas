1cc300aefa6ad57404d21ac38153b1f22cf9b0d7
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-31 16:51:18 -0800

    Update Model PHPDoc

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -8,14 +8,24 @@ use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 /**
- * @property  IdeaStatus  $status
+ * @property  int          $id
+ * @property  string       $description  The idea description.
+ * @property  IdeaStatus   $status       The idea status.
+ * @property  Carbon|null  $created_at
+ * @property  Carbon|null  $updated_at
  *
  * @method  static  IdeaFactory           factory($count = null, $state = [])
  * @method  static  Builder<static>|Idea  newModelQuery()
  * @method  static  Builder<static>|Idea  newQuery()
  * @method  static  Builder<static>|Idea  query()
+ * @method  static  Builder<static>|Idea  whereCreatedAt($value)
+ * @method  static  Builder<static>|Idea  whereDescription($value)
+ * @method  static  Builder<static>|Idea  whereId($value)
+ * @method  static  Builder<static>|Idea  whereStatus($value)
+ * @method  static  Builder<static>|Idea  whereUpdatedAt($value)
  *
  * @mixin Eloquent
  */
