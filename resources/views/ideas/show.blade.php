4f003a0f2b548cf0855938543cb43d29f23a8640
Denim Datta 👻 <d.dat08@gmail.com>
2026-01-31 18:47:55 -0800

    Add Back Link in Idea Show Page

--- resources/views/ideas/show.blade.php
+++ resources/views/ideas/show.blade.php
@@ -1,3 +1,12 @@
 <x-layout>
+    <div class="mb-4">
+        <a href="{{ url('/ideas') }}"
+           aria-label="Back to ideas"
+           class="inline-flex items-center text-gray-600 hover:text-gray-900">
+            <span class="mr-2 text-lg" aria-hidden="true">←</span>
+            <span>Back to ideas</span>
+        </a>
+    </div>
+
     <x-json-card :data="$idea" />
 </x-layout>
