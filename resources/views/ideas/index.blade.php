c6a5e176cff4969133fd2b1576a2ece9ad1ce444
Denim Datta 👻 <d.dat08@gmail.com>
2026-02-04 21:28:45 -0800

    Update Visual
    Show Idea Items in Card

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -37,13 +37,25 @@
             @if($ideas->isEmpty())
                 <p>No ideas yet.</p>
             @else
-                <ul class="mt-6">
+                <ul class="mt-6 grid grid-cols-2 gap-x-6 gap-y-4">
                     @foreach($ideas as $idea)
-                        <li class="text-sm mt-2">
-                            {{ $idea->title }}
-                            [{{ $idea->status->getDisplayName() }}]
-                            <a href="{{ route('ideas.show', $idea) }}" class="underline">View</a>
-                        </li>
+                        <div class="card bg-sky-200 shadow-sm">
+                            <div class="card-body">
+                                <h2 class="card-title">{{ $idea->title }}</h2>
+                                <p class="card-description p-2 border-2 border-indigo-300 rounded-lg w-full">
+                                    {{ $idea->description }}
+                                </p>
+
+                                <div class="grid grid-cols-2 gap-2 mt-4">
+                                    <small class="p-2 border-2 border-blue-400 rounded-lg w-fit justify-self-start">
+                                        {{ $idea->status->getDisplayName() }}
+                                    </small>
+                                    <a class="btn btn-primary w-fit justify-self-end" href="{{ route('ideas.show', $idea) }}">
+                                        View
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     @endforeach
                 </ul>
             @endif
