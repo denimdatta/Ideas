390939d07d49a67345a5479170692e20fd30fe62
Denim Datta 👻 <d.dat08@gmail.com>
2026-02-04 21:34:32 -0800

    Move Idea Card to Component

---
+++ resources/views/components/idea-card.blade.php
@@ -0,0 +1,17 @@
+<div class="card bg-sky-200 shadow-sm">
+    <div class="card-body">
+        <h2 class="card-title">{{ $idea->title }}</h2>
+        <p class="card-description p-2 border-2 border-indigo-300 rounded-lg w-full">
+            {{ $idea->description }}
+        </p>
+
+        <div class="grid grid-cols-2 gap-2 mt-4">
+            <small class="p-2 border-2 border-blue-400 rounded-lg w-fit justify-self-start">
+                {{ $idea->status->getDisplayName() }}
+            </small>
+            <a class="btn btn-primary w-fit justify-self-end" href="{{ route('ideas.show', $idea) }}">
+                View
+            </a>
+        </div>
+    </div>
+</div>
--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -39,23 +39,7 @@
             @else
                 <ul class="mt-6 grid grid-cols-2 gap-x-6 gap-y-4">
                     @foreach($ideas as $idea)
-                        <div class="card bg-sky-200 shadow-sm">
-                            <div class="card-body">
-                                <h2 class="card-title">{{ $idea->title }}</h2>
-                                <p class="card-description p-2 border-2 border-indigo-300 rounded-lg w-full">
-                                    {{ $idea->description }}
-                                </p>
-
-                                <div class="grid grid-cols-2 gap-2 mt-4">
-                                    <small class="p-2 border-2 border-blue-400 rounded-lg w-fit justify-self-start">
-                                        {{ $idea->status->getDisplayName() }}
-                                    </small>
-                                    <a class="btn btn-primary w-fit justify-self-end" href="{{ route('ideas.show', $idea) }}">
-                                        View
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+                        <x-idea-card :idea="$idea" />
                     @endforeach
                 </ul>
             @endif
