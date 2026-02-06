27fc97af12f57af268da46209627848f68212f36
Denim Datta 👻 <d.dat08@gmail.com>
2026-02-06 01:17:55 -0800

    Auth Auth Middleware for Idea Routes

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,14 @@ Route::get('/', function () {
     return view('index');
 })->name('home');
 
-Route::get('/ideas', [IdeaController::class, 'index'])->name('ideas.index');
-Route::delete('/ideas', [IdeaController::class, 'destroyAll'])->name('ideas.destroy.all');
-Route::get('/ideas/create', [IdeaController::class, 'create'])->name('ideas.create');
-Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store');
-Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
-Route::patch('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update');
-Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+Route::get('/ideas', [IdeaController::class, 'index'])->name('ideas.index')->middleware('auth');
+Route::delete('/ideas', [IdeaController::class, 'destroyAll'])->name('ideas.destroy.all')->middleware('auth');
+Route::get('/ideas/create', [IdeaController::class, 'create'])->name('ideas.create')->middleware('auth');
+Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store')->middleware('auth');
+Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show')->middleware('auth');
+Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit')->middleware('auth');
+Route::patch('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update')->middleware('auth');
+Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy')->middleware('auth');
 
 Route::get('/register', [RegisterController::class, 'create'])->name('user.create');
 Route::post('/register', [RegisterController::class, 'store'])->name('user.store');
