d94df58143093d7a98e1a3164b4f14214d05d7a7
Denim Datta 👻 <d.dat08@gmail.com>
2026-02-05 20:09:24 -0800

    Add Validation Message in Registration Form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,15 +9,17 @@
                 <div class="flex flex-col col-span-2 space-y-1">
                     <label class="label" for="username">Username</label>
                     <input id="username" name="username" class="input w-full" placeholder="Username" value="{{ old('username') }}" required />
+                    <x-forms.error name="username"/>
                 </div>
-
-                <div class="flex flex-col space-y-1">
+                <div class="flex flex-col col-span-2 space-y-1">
                     <label class="label" for="first_name">First Name</label>
-                    <input id="first_name" name="first_name" class="input" placeholder="Given Name" value="{{ old('first_name') }}" required />
+                    <input id="first_name" name="first_name" class="input w-full" placeholder="Given Name" value="{{ old('first_name') }}" required />
+                    <x-forms.error name="first_name"/>
                 </div>
-                <div class="flex flex-col space-y-1">
+                <div class="flex flex-col col-span-2 space-y-1">
                     <label class="label" for="last_name">Last Name</label>
-                    <input id="last_name" name="last_name" class="input" placeholder="Family Name" value="{{ old('last_name') }}" />
+                    <input id="last_name" name="last_name" class="input w-full" placeholder="Family Name" value="{{ old('last_name') }}" />
+                    <x-forms.error name="last_name"/>
                 </div>
                 <div class="flex flex-col space-y-1">
                     <label class="label" for="email">Email</label>
@@ -27,6 +29,9 @@
                     <label class="label" for="email_confirmation">Confirm Email</label>
                     <input id="email_confirmation" name="email_confirmation" type="email" class="input" placeholder="Confirm Email" required />
                 </div>
+                <div class="flex flex-col col-span-2 space-y-1">
+                    <x-forms.error name="email"/>
+                </div>
                 <div class="flex flex-col space-y-1">
                     <label class="label" for="password">Password</label>
                     <input id="password" name="password" type="password" class="input" placeholder="Password" required />
@@ -34,6 +39,9 @@
                 <div class="flex flex-col space-y-1">
                     <label class="label" for="password_confirmation">Confirm Password</label>
                     <input id="password_confirmation" name="password_confirmation" type="password" class="input" placeholder="Confirm Password" required />
+                </div>
+                <div class="flex flex-col col-span-2 space-y-1">
+                    <x-forms.error name="password"/>
                 </div>
             </div>
 
