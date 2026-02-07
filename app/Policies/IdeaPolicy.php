526ca9662e9df10e5dbbe0a4212b0f7a6558c9eb
Denim Datta 👻 <d.dat08@gmail.com>
2026-02-07 04:09:57 -0800

    Add View Only Policy for Idea

--- app/Policies/IdeaPolicy.php
+++ app/Policies/IdeaPolicy.php
@@ -8,10 +8,18 @@ use App\Models\User;
 class IdeaPolicy
 {
     /**
-     * Determine whether the user can access the model element.
+     * Determine whether the user have full access the model element.
      */
     public function access(User $user, Idea $idea): bool
     {
         return $user->is($idea->user);
+    }
+
+    /**
+     * Determine whether the user can view the model element.
+     */
+    public function view(User $user, Idea $idea): bool
+    {
+        return true;
     }
 }
